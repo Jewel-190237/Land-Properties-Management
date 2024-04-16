ca952090101aecdf60a826eda81bde43405422f7
Jewel-190237 <190237@ku.ac.bd>
2024-04-16 07:01:29 +0600

    Land Card Updated

--- src/Components/Layouts/Land.jsx
+++ src/Components/Layouts/Land.jsx
@@ -4,17 +4,21 @@ import { Link } from "react-router-dom";
 
 const Land = ({ land }) => {
 
-    const { title, image, description, id } = land;
+    const { title, image, description, id, discount } = land;
     return (
         <div>
             <div className="card bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
-                    <img src={image}alt="Land Images" className="rounded-xl" />
+                    <img src={image} alt="Land Images" className="rounded-xl" />
                 </figure>
                 <div className="card-body items-center text-center">
-                    <h2 className="card-title">{title}</h2>
+                    <h2 className="card-title text-2xl">{title}</h2>
                     <p>{description}</p>
-                    <div className="card-actions">
+                    <h2 className="card-title">
+                        Discount
+                        <div className="badge badge-secondary">{discount}</div>
+                    </h2>
+                    <div className="card-actions mt-2">
                         <Link to={`/Land/${id}`}><button className="btn btn-primary">View Details</button></Link>
                     </div>
                 </div>
