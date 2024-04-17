60bfe6b224ec8541e53ab9730d4aab2bb8ca05dc
Jewel-190237 <jewel190237@gmail.com>
2024-04-17 23:37:41 +0600

    profile updated

--- src/Components/Layouts/Profile.jsx
+++ src/Components/Layouts/Profile.jsx
@@ -1,27 +1,44 @@
 import auth from "../../Firebase/firebase.config";
 import UseTitle from "../Title/UseTitle";
+import userPhoto from '../../assets/user.png'
+
 
 const Profile = () => {
 
+
     UseTitle("Profile")
-    
+
     const userInfo = auth.currentUser;
     console.log(userInfo)
-    
-        return (
-            <div>
-                <div className="hero bg-base-200">
-                    <div className="hero-content flex-col lg:flex-row">
-                        <img src={userInfo.photoURL} className="max-w-sm rounded-lg shadow-2xl" />
-                        <div>
-                            <h1 className="text-2xl font-bold">{userInfo.displayName}</h1>
-                            <p className="py-2">{userInfo.email}</p>
-                        </div>
-                    </div>  
-                </div>
-                <button className="btn btn-primary mx-auto items-center">Update Profile</button>
-            </div>
-        );
-    };
 
-    export default Profile;
+    return (
+        <div>
+            <div className="card bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    {
+                        userInfo.photoURL ?
+                            <img src={userInfo.photoURL} alt="User" className="rounded-xl" />
+                            :
+                            <img alt="user" src={userPhoto} />
+                    }
+
+                </figure>
+                <div className="card-body items-center text-center">
+                    {
+                        userInfo.displayName ?
+                            <h2 className="card-title">{userInfo.displayName}</h2>
+                            :
+                            <h2 className="card-title">User Name</h2>
+
+                    }
+                    <p>{userInfo.email}</p>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">Update Profile</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
